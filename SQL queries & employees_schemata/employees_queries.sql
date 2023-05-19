fb90fba24cd5c5440f6858592684ebaa2d1dfefe
tattwamasiray <125114916+tattwamasiray@users.noreply.github.com>
2023-05-19 22:44:29 +1000

    Add files via upload

---
+++ SQL queries & employees_schemata/employees_queries.sql
@@ -0,0 +1,49 @@
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+From employees e
+Join salaries s
+ON e.emp_no = s.emp_no;
+
+-- first name, last name, and hire date for employees hired in 1986
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31'
+ORDER BY hire_date ASC;
+
+-- managers in each department (dept number, dept name, emp number, last name, first name)
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
+FROM dept_manager dm
+JOIN employees e
+ON dm.emp_no = e.emp_no
+JOIN departments d
+ON dm.dept_no = d.dept_no
+ORDER BY d.dept_name ASC;
+
+-- first name, last name, and sex for employees named "Hercules B."
+SELECT first_name, last_name, sex
+FROM employees 
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+ORDER BY last_name ASC;
+-- employees in Sales and Development departments (emp no, last name, first name, dept name)
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de 
+ON e.emp_no = de.emp_no
+JOIN departments d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+ORDER BY d.dept_name ASC;
+-- frequency count of employee last names
+SELECT last_name, count(emp_no) as num_employees_with_same_last_name
+FROM employees
+GROUP BY last_name
+ORDER BY num_employees_with_same_last_name DESC;
+
+-- create view for bonus bar chard of avg salary by title
+CREATE VIEW avg_salaries_by_title AS
+SELECT round(avg(s.salary)) as avg_salary, t.title
+FROM titles t
+JOIN employees e
+ON t.title_id = e.emp_title_id
+JOIN salaries s
+ON e.emp_no = s.emp_no
+GROUP BY t.title;
